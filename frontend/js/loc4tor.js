b1474ce99d4fbc1bad7ffb985825aa98b7ed6ebc
robertedelmann <61145031+robertedelmann@users.noreply.github.com>
2020-03-26 15:47:24 +0100

    dfasdf
    kjl

--- frontend/js/loc4tor.js
+++ frontend/js/loc4tor.js
@@ -1,3 +1,35 @@
+function dateiauswahl(evt) {
+    var dateien = evt.target.files; // FileList object
+
+    // Auslesen der gespeicherten Dateien durch Schleife
+    for (var i = 0, f; f = dateien[i]; i++) {
+
+      // nur Bild-Dateien
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      var reader = new FileReader();
+
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // erzeuge Thumbnails.
+          var vorschau = document.createElement('img');
+          vorschau.className = 'thumb';
+          vorschau.src   = e.target.result;
+          vorschau.title = theFile.name;
+          document.getElementById('list').insertBefore(vorschau, null);
+        };
+      })(f);
+
+      // Bilder als Data URL auslesen.
+      reader.readAsDataURL(f);
+    }
+  }
+  // Auf neue Auswahl reagieren und gegebenenfalls Funktion dateiauswahl neu ausführen.
+  document.getElementById('files').addEventListener('change', dateiauswahl, false);
+
+
 function processImage(listAttribute) {
     // Replace <Subscription Key> with your valid subscription key.
     var subscriptionKey = "6d9a05bde1cd46338e508eb44b8d969b";
@@ -20,36 +52,3 @@ function processImage(listAttribute) {
 
 
 };
-
-function getAttribute(data, listAttribute) {
-    var jsonObject = JSON.parse(data);
-    //return jsonObject[0].faceAttributes.gender;
-
-    switch (listAttribute) {
-        case "sex":
-            if (jsonObject[0].faceAttributes.gender = "female") {
-                return "Die Person ist weiblich.";
-            } else if (jsonObject[0].faceAttributes.gender = "male") {
-                return "Die Person ist männlich.";
-            } else {
-                return "Geschlecht kann nicht bestimmt werden.";
-            }
-        case "age":
-            return "Die Person ist " + jsonObject[0].faceAttributes.age + " Jahre alt";
-        case "haircolour":
-            var hairColours = jsonObject[0].faceAttributes.hair.hairColor;
-            var colourObject = {
-                "color": "none",
-                "confidence": 0.0
-            };
-            hairColours.forEach(element => {
-                    if (element.confidence > colourObject.confidence) {
-                        colourObject = element;
-                    }
-                }
-            )
-            return "Die Haarfarbe ist " + colourObject.color + ".";
-        default:
-            return "Fehler bei Ausführung."
-    }
-}
