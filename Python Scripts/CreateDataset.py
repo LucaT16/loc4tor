13947166167203083b0d0444575aa9e152845834
LucaT16 <lucatallarita@googlemail.com>
2020-03-27 10:42:10 +0100

    Create Dataset ausgelagert

---
+++ Python Scripts/CreateDataset.py
@@ -0,0 +1,46 @@
+import os
+import cv2
+import random
+import numpy as np
+import pickle
+
+DATADIR = "../Bilder/"
+CATEGORIES = ["Freiheitstatue","GoldenGateBridge"]
+CROPPED = "Cropped"
+IMG_SIZE = 244
+training_data = []
+
+#loop over relevant images
+for category in CATEGORIES:
+    path = os.path.join(DATADIR,category)
+    path = path + CROPPED
+    class_num = CATEGORIES.index(category)
+    for img in os.listdir(path):
+        try:
+            #resize the image and save it in training_data
+            img_array = cv2.imread(os.path.join(path,img))
+            resized_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+            training_data.append([resized_array, class_num])
+        except Exception as e:
+            print(e)
+random.shuffle(training_data)
+
+#create pickle datasets
+x = []
+y = []
+
+for features, label in training_data:
+    x.append(features)
+    y.append(label)
+
+x = np.array(x).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+y = np.array(y)
+
+pickle_out = open("x.pickle", "wb")
+pickle.dump(x, pickle_out)
+pickle_out.close()
+print("x.pickle created!")
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+print("y.pickle created!")
